f8ab00fadd91aacb28f260878c3bcd9e3137d143
Imee Cuison <imeetwelve@gmail.com>
2015-01-22 14:39:53 -0500

    yoda

---
+++ index.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+var $body = $("body");
+
+//$("body").click(function (event){
+///  $(this).css();
+
+//});
+
+
+$(".box").eq(0).mouseover(function(event){
+  $(this).css("transform", "scale(2.0)");
+
+});
+
+$(".box").eq(0).mouseout(function(event){
+  $(this).css("transform", "scale(1.0)");
+
+});
+
+$(".box").eq(1).click(function(event){
+  $(this).prepend('<img id="theImg" src="images/yodaKitty.jpg"/>');
+  $(this).addClass("small");
+    ///prepend('<img id="theImg" src="theImg.png" />')
+});
+
+
+});
